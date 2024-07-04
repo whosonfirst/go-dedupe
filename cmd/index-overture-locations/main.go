6e1c024fd4c9fd9c271ba0d9a05c338f30b0052b
sfomuseumbot <sfomuseumbot@localhost>
2024-07-03 17:39:56 -0700

    basic bleve location index/query

---
+++ cmd/index-overture-locations/main.go
@@ -0,0 +1,154 @@
+package main
+
+// go run cmd/index-overture-venues/main.go /usr/local/data/overture/places-geojson/venues-0.95.geojsonl.bz2
+
+import (
+	"context"
+	"flag"
+	_ "fmt"
+	"log"
+	"log/slog"
+	"os"
+	"sync"
+
+	"github.com/aaronland/go-jsonl/walk"
+	"github.com/aaronland/gocloud-blob/bucket"
+	"github.com/sfomuseum/go-timings"
+	"github.com/whosonfirst/go-dedupe"
+	"github.com/whosonfirst/go-dedupe/location"
+	_ "github.com/whosonfirst/go-dedupe/overture"
+	"github.com/whosonfirst/go-dedupe/parser"
+	"github.com/whosonfirst/go-overture/geojsonl"
+	_ "gocloud.dev/blob/fileblob"
+)
+
+func main() {
+
+	var database_uri string
+	var parser_uri string
+	var monitor_uri string
+	var bucket_uri string
+	var is_bzipped bool
+
+	var start_after int
+
+	flag.StringVar(&database_uri, "database-uri", "", "...")
+	flag.StringVar(&parser_uri, "parser-uri", "overtureplaces://", "...")
+	flag.StringVar(&monitor_uri, "monitor-uri", "counter://PT60S", "...")
+	flag.StringVar(&bucket_uri, "bucket-uri", "file:///", "...")
+	flag.BoolVar(&is_bzipped, "is-bzip2", true, "...")
+	flag.IntVar(&start_after, "start-after", 0, "...")
+
+	flag.Parse()
+
+	uris := flag.Args()
+
+	ctx := context.Background()
+
+	db, err := location.NewDatabase(ctx, database_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to create new database, %v", err)
+	}
+
+	defer db.Close(ctx)
+
+	prsr, err := parser.NewParser(ctx, parser_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to create new parser, %v", err)
+	}
+
+	source_bucket, err := bucket.OpenBucket(ctx, bucket_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to open source bucket, %v", err)
+	}
+
+	defer source_bucket.Close()
+
+	monitor, err := timings.NewMonitor(ctx, monitor_uri)
+
+	if err != nil {
+		log.Fatalf("Failed to create monitor, %v", err)
+	}
+
+	monitor.Start(ctx, os.Stderr)
+	defer monitor.Stop(ctx)
+
+	max_workers := 20
+	throttle := make(chan bool, max_workers)
+
+	for i := 0; i < max_workers; i++ {
+		throttle <- true
+	}
+
+	wg := new(sync.WaitGroup)
+
+	walk_cb := func(ctx context.Context, path string, rec *walk.WalkRecord) error {
+
+		if start_after > 0 && rec.LineNumber < start_after {
+			monitor.Signal(ctx)
+			return nil
+		}
+
+		<-throttle
+
+		wg.Add(1)
+
+		go func(path string, rec *walk.WalkRecord) {
+
+			logger := slog.Default()
+			logger = logger.With("path", path)
+			logger = logger.With("line number", rec.LineNumber)
+
+			defer func() {
+				wg.Done()
+				throttle <- true
+				// logger.Info("Done")
+			}()
+
+			loc, err := prsr.Parse(ctx, rec.Body)
+
+			if dedupe.IsInvalidRecordError(err) {
+				logger.Warn("Invalid record")
+				return
+			} else if err != nil {
+				logger.Error("Failed to parse record", "error", err)
+				return
+				// return fmt.Errorf("Failed to parse body, %w", err)
+			}
+
+			logger = logger.With("id", loc.ID)
+			logger = logger.With("location", loc)
+
+			err = db.AddLocation(ctx, loc)
+
+			if err != nil {
+				logger.Error("Failed to add record", "error", err)
+				return
+				// return err
+			}
+
+			// logger.Info("OK", "geohash", loc.Geohash())
+			monitor.Signal(ctx)
+
+		}(path, rec)
+
+		return nil
+	}
+
+	walk_opts := &geojsonl.WalkOptions{
+		SourceBucket: source_bucket,
+		Callback:     walk_cb,
+		IsBzipped:    is_bzipped,
+	}
+
+	err = geojsonl.Walk(ctx, walk_opts, uris...)
+
+	if err != nil {
+		log.Fatalf("Failed to walk, %v", err)
+	}
+
+	wg.Wait()
+}
---
+++ location/bleve.go
@@ -0,0 +1,192 @@
+package location
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/url"
+	"os"
+
+	"github.com/blevesearch/bleve/v2"
+	"github.com/blevesearch/bleve/v2/mapping"
+	"github.com/blevesearch/bleve/v2/search"
+)
+
+type BleveDatabase struct {
+	index bleve.Index
+}
+
+type bleveDocument struct {
+	Id       string `json:"id"`
+	Geohash  string `json:"geohash"`
+	Location string `json:"location"`
+}
+
+func init() {
+	ctx := context.Background()
+	err := RegisterDatabase(ctx, "bleve", NewBleveDatabase)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewBleveDatabase(ctx context.Context, uri string) (Database, error) {
+
+	u, err := url.Parse(uri)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse URI, %w", err)
+	}
+
+	var idx bleve.Index
+	db_path := u.Path
+
+	_, err = os.Stat(db_path)
+
+	if err == nil {
+
+		v, err := bleve.Open(db_path)
+
+		if err != nil {
+			return nil, fmt.Errorf("Failed to open '%s', %w", db_path, err)
+		}
+
+		idx = v
+
+	} else {
+
+		textFieldMapping := mapping.NewTextFieldMapping()
+
+		bleveMapping := bleve.NewIndexMapping()
+		bleveMapping.DefaultMapping.AddFieldMappingsAt("geohash", textFieldMapping)
+
+		v, err := bleve.New(db_path, bleveMapping)
+
+		if err != nil {
+			return nil, fmt.Errorf("Failed to create db at '%s', %w", db_path, err)
+		}
+
+		idx = v
+	}
+
+	db := &BleveDatabase{
+		index: idx,
+	}
+
+	return db, nil
+}
+
+func (db *BleveDatabase) AddLocation(ctx context.Context, loc *Location) error {
+
+	id := loc.ID
+	geohash := loc.Geohash()
+
+	enc_loc, err := json.Marshal(loc)
+
+	if err != nil {
+		return fmt.Errorf("Failed to marshal location, %w", err)
+	}
+
+	doc := bleveDocument{
+		Id:       id,
+		Geohash:  geohash,
+		Location: string(enc_loc),
+	}
+
+	slog.Info("Add", "id", loc.ID, "geohash", geohash)
+	return db.index.Index(doc.Id, doc)
+}
+
+func (db *BleveDatabase) GetById(ctx context.Context, id string) (*Location, error) {
+
+	q := bleve.NewMatchQuery(id)
+	// q := bleve.NewDocIDQuery([]string{id})
+
+	req := bleve.NewSearchRequest(q)
+	req.Fields = []string{"location"}
+
+	rsp, err := db.index.Search(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// slog.Info("Q", "id", id, "total", rsp.Total, "status", rsp.Status, "hits", rsp.Hits)
+
+	if rsp.Total == 0 {
+		return nil, nil
+	}
+
+	loc, err := db.locationFromDocumentMatch(rsp.Hits[0])
+
+	if err != nil {
+		return nil, err
+	}
+
+	return loc, nil
+}
+
+func (db *BleveDatabase) GetWithGeohash(ctx context.Context, geohash string, cb GetWithGeohashCallback) error {
+
+	q := bleve.NewMatchQuery(geohash)
+
+	req := bleve.NewSearchRequest(q)
+	req.Fields = []string{"location"}
+
+	rsp, err := db.index.Search(req)
+
+	if err != nil {
+		return err
+	}
+
+	slog.Info("Q", "geohash", geohash, "total", rsp.Total, "status", rsp.Status)
+
+	for _, m := range rsp.Hits {
+
+		loc, err := db.locationFromDocumentMatch(m)
+
+		if err != nil {
+			return err
+		}
+
+		if loc.Geohash() != geohash {
+			continue
+		}
+
+		err = cb(ctx, loc)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (db *BleveDatabase) Close(ctx context.Context) error {
+	return nil
+}
+
+func (db *BleveDatabase) locationFromDocumentMatch(m *search.DocumentMatch) (*Location, error) {
+
+	fields := m.Fields
+
+	v, exists := fields["location"]
+
+	if !exists {
+		return nil, fmt.Errorf("Match is missing location field")
+	}
+
+	enc_loc := v.(string)
+	var loc *Location
+
+	err := json.Unmarshal([]byte(enc_loc), &loc)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return loc, nil
+}
