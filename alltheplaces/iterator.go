10d42bec284fa5958eb963c223ea25f14a7578d0
sfomuseumbot <sfomuseumbot@localhost>
2024-08-07 18:33:51 -0700

    flesh out ovtr, atp iterators; incomplete

--- alltheplaces/iterator.go
+++ alltheplaces/iterator.go
@@ -3,12 +3,18 @@ package alltheplaces
 import (
 	"context"
 	"fmt"
+	"io"
+	"log/slog"
+	"os"
+	"sync"
 
+	"github.com/paulmach/orb/geojson"
 	"github.com/whosonfirst/go-dedupe/iterator"
 )
 
 type AllThePlacesIterator struct {
 	iterator.Iterator
+	max_workers int
 }
 
 func init() {
@@ -21,11 +27,81 @@ func init() {
 
 func NewAllThePlacesIterator(ctx context.Context, uri string) (iterator.Iterator, error) {
 
-	i := &AllThePlacesIterator{}
-	return i, nil
+	max_workers := 20
+
+	iter := &AllThePlacesIterator{
+		max_workers: max_workers,
+	}
+
+	return iter, nil
 }
 
-func (i *AllThePlacesIterator) IterateWithCallback(ctx context.Context, cb iterator.IteratorCallback, uris ...string) error {
+func (iter *AllThePlacesIterator) IterateWithCallback(ctx context.Context, cb iterator.IteratorCallback, uris ...string) error {
 
-	return fmt.Errorf("Not implemeted")
+	throttle := make(chan bool, iter.max_workers)
+
+	for i := 0; i < iter.max_workers; i++ {
+		throttle <- true
+	}
+
+	wg := new(sync.WaitGroup)
+
+	for _, path := range uris {
+
+		r, err := os.Open(path)
+
+		if err != nil {
+			return fmt.Errorf("Failed to open %s for reading, %v", path, err)
+		}
+
+		defer r.Close()
+
+		body, err := io.ReadAll(r)
+
+		if err != nil {
+			return fmt.Errorf("Failed to read %s, %v", path, err)
+		}
+
+		slog.Info("Unmarshal", "path", path)
+
+		fc, err := geojson.UnmarshalFeatureCollection(body)
+
+		if err != nil {
+			slog.Warn("Failed to unmarshal feature collection", "path", path, "error", err)
+			continue
+		}
+
+		for _, f := range fc.Features {
+
+			<-throttle
+
+			wg.Add(1)
+
+			go func(f *geojson.Feature) {
+
+				defer func() {
+					wg.Done()
+					throttle <- true
+				}()
+
+				body, err := f.MarshalJSON()
+
+				if err != nil {
+					slog.Error("Failed to marshal record", "error", err)
+					return
+				}
+
+				err = cb(ctx, body)
+
+				if err != nil {
+					slog.Error("Callback failed for record", "error", err)
+					return
+				}
+			}(f)
+		}
+	}
+
+	wg.Done()
+
+	return nil
 }
--- overture/iterator.go
+++ overture/iterator.go
@@ -2,13 +2,23 @@ package overture
 
 import (
 	"context"
-	"fmt"
+	// "fmt"
+	"log/slog"
+	"sync"
 
+	"github.com/aaronland/go-jsonl/walk"
+	// "github.com/aaronland/gocloud-blob/bucket"
 	"github.com/whosonfirst/go-dedupe/iterator"
+	"github.com/whosonfirst/go-overture/geojsonl"
+	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/fileblob"
 )
 
 type OvertureIterator struct {
 	iterator.Iterator
+	source_bucket *blob.Bucket
+	is_bzipped    bool
+	max_workers   int
 }
 
 func init() {
@@ -21,11 +31,73 @@ func init() {
 
 func NewOvertureIterator(ctx context.Context, uri string) (iterator.Iterator, error) {
 
-	i := &OvertureIterator{}
-	return i, nil
+	max_workers := 20
+
+	iter := &OvertureIterator{
+		max_workers: max_workers,
+	}
+
+	return iter, nil
 }
 
-func (i *OvertureIterator) IterateWithCallback(ctx context.Context, cb iterator.IteratorCallback, uris ...string) error {
+func (iter *OvertureIterator) IterateWithCallback(ctx context.Context, cb iterator.IteratorCallback, uris ...string) error {
 
-	return fmt.Errorf("Not implemeted")
+	throttle := make(chan bool, iter.max_workers)
+
+	for i := 0; i < iter.max_workers; i++ {
+		throttle <- true
+	}
+
+	wg := new(sync.WaitGroup)
+
+	walk_cb := func(ctx context.Context, path string, rec *walk.WalkRecord) error {
+
+		/*
+			if start_after > 0 && rec.LineNumber < start_after {
+				monitor.Signal(ctx)
+				return nil
+			}
+		*/
+
+		<-throttle
+
+		wg.Add(1)
+
+		go func(path string, rec *walk.WalkRecord) {
+
+			logger := slog.Default()
+			logger = logger.With("path", path)
+			logger = logger.With("line number", rec.LineNumber)
+
+			defer func() {
+				wg.Done()
+				throttle <- true
+			}()
+
+			err := cb(ctx, rec.Body)
+
+			if err != nil {
+				logger.Error("Iterator callback for record failed", "error", err)
+			}
+
+		}(path, rec)
+
+		return nil
+	}
+
+	walk_opts := &geojsonl.WalkOptions{
+		SourceBucket: iter.source_bucket,
+		Callback:     walk_cb,
+		IsBzipped:    iter.is_bzipped,
+	}
+
+	err := geojsonl.Walk(ctx, walk_opts, uris...)
+
+	if err != nil {
+		return err
+	}
+
+	wg.Wait()
+
+	return nil
 }
