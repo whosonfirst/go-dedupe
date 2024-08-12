fd3b41a56fdea31444a39fd2da5825e23a132e93
sfomuseumbot <sfomuseumbot@localhost>
2024-08-12 11:54:44 -0700

    add stubs/links for ilms

---
+++ ilms/ilms.go
@@ -0,0 +1,5 @@
+package ilms
+
+// https://www.imls.gov/research-evaluation/data-collection/museum-data-files
+// https://www.imls.gov/sites/default/files/2018_csv_museum_data_files.zip
+// https://www.imls.gov/sites/default/files/museum_data_file_documentation_and_users_guide.pdf
