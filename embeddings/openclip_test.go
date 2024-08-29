ce8271e4691352dabe264f7c888dff094e6205ea
sfomuseumbot <sfomuseumbot@localhost>
2024-08-29 12:37:50 -0700

    add openclip embedder

---
+++ embeddings/openclip_test.go
@@ -0,0 +1,68 @@
+//go:build openclip
+
+package embeddings
+
+import (
+	"context"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestOpenCLIPEmbeddings(t *testing.T) {
+
+	ctx := context.Background()
+
+	emb, err := NewEmbedder(ctx, "openclip://")
+
+	if err != nil {
+		t.Fatalf("Failed to create embedder, %v", err)
+	}
+
+	rsp, err := emb.Embeddings(ctx, "Hello world")
+
+	if err != nil {
+		t.Fatalf("Failed to derive embeddings, %v", err)
+	}
+
+	if len(rsp) == 0 {
+		t.Fatalf("Empty embedding")
+	}
+}
+
+func TestOpenCLIPImageEmbeddings(t *testing.T) {
+
+	ctx := context.Background()
+
+	emb, err := NewEmbedder(ctx, "openclip://")
+
+	if err != nil {
+		t.Fatalf("Failed to create embedder, %v", err)
+	}
+
+	im_path := "../fixtures/1527845303_walrus.jpg"
+
+	im_r, err := os.Open(im_path)
+
+	if err != nil {
+		t.Fatalf("Failed to open %s for reading, %v", im_path, err)
+	}
+
+	defer im_r.Close()
+
+	im_body, err := io.ReadAll(im_r)
+
+	if err != nil {
+		t.Fatalf("Failed to read data from %s, %v", im_path, err)
+	}
+
+	rsp, err := emb.ImageEmbeddings(ctx, im_body)
+
+	if err != nil {
+		t.Fatalf("Failed to derive embeddings, %v", err)
+	}
+
+	if len(rsp) == 0 {
+		t.Fatalf("Empty embedding")
+	}
+}
